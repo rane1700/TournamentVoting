aa4ce5f63468ec9d5f8c35d740c722ff408f342a
rane1700 <ranee1700@gmail.com>
2018-01-01 23:00:31 +0200

    random pairing

--- TournamentVoting/Scripts/PreTesting/ExperimenterProfile.js
+++ TournamentVoting/Scripts/PreTesting/ExperimenterProfile.js
@@ -1,10 +1,12 @@
 ﻿
-function myFunction() {
+function myFunction(event) {
+    event.preventDefault();
     var age = $("#age").val()
     var gender = $("#gender").val()
     var education = $("#educationLevel").val()
     localStorage.setItem("age", age);
     localStorage.setItem("gender", gender)
     localStorage.setItem("educationLevel", education);
+    window.location.href = "../../Views/PreTesting/PreTestingRanking.html"
 
 }
